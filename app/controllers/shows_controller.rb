681d2d3e039725183878fcc8da67a865981afc54
siansrd <sianrobinsondavies@gmail.com>
2016-10-24 19:19:20 +0100

    Add show action

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -5,4 +5,19 @@ class ShowsController < ApplicationController
     render :json => @shows.as_json
   end
 
+  def show
+    @show = Show.find(params[:id])
+    render :json => @show
+  end
+
+  def create
+    show = Show.create( show_params )
+    render :json => show
+  end
+
+  private 
+  def show_params
+    params.require(:show).permit([:title])
+  end
+
 end
